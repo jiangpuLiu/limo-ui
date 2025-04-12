0eda6c83ac79a82299ba0fe0bbff4a248cee4465
liujiangpu <13266221244@163.com>
2025-04-12 14:13:06 +0800

    feat: 配置更改

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,8 @@ pnpm i --frozen-lockfile
 pnpm build
 
 # 使用环境变量中的 registry 地址
-REGISTRY_URL="http://nexus.limonergy.com:8081/repository/limo-npm/"
+#REGISTRY_URL="http://nexus.limonergy.com:8081/repository/limo-npm/"
+REGISTRY_URL="https://registry.npmjs.org/"
 
 # 发布到指定的 registry
 cd ../dist/limo-ui
