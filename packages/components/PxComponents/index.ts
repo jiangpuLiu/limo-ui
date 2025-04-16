464ca792d8f01f80d53a18a62845c8b7f200c196
zsy <zhangshangyou@limonergy.com>
2025-04-16 16:35:47 +0800

    fix(components): :bug: [PxComponents]

--- packages/components/PxComponents/index.ts
+++ packages/components/PxComponents/index.ts
@@ -1,12 +1,12 @@
 import { withInstall } from '@limo-ui/utils'
 import Dialog from './Dialog/index.vue'
 import Empty from './PxEmpty/index.vue'
-// import Table from './PxTable/index.vue'
+import Table from './PxTable/index.vue'
 import type { SFCWithInstall } from '@limo-ui/utils'
 
 export const PxDialog: SFCWithInstall<typeof Dialog> = withInstall(Dialog)
 export const PxEmpty: SFCWithInstall<typeof Empty> = withInstall(Empty)
-// export const PxTable: SFCWithInstall<typeof Table> = withInstall(Table)
+export const PxTable: SFCWithInstall<typeof Table> = withInstall(Table)
 export type { DialogInstance } from './Dialog/instance'
 export type { PxEmptyInstance } from './PxEmpty/instance'
-// export type { PxTableInstance } from './PxTable/instance'
+export type { PxTableInstance } from './PxTable/instance'
